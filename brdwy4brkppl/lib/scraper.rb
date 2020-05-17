c19b983424bb1bee422d71ced0e06ceacb34bb9e
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-05-17 18:58:12 +0000

    updated cli & scraper

--- brdwy4brkppl/lib/scraper.rb
+++ brdwy4brkppl/lib/scraper.rb
@@ -2,16 +2,7 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-
-  def make_show
-    #shows_array will return the scraped/parsed index page of all shows
-    #it returns the nokogiri object(array thing)
-    show_array = Scraper.scrape_mainpage(BASE_PATH + 'index.html')
-    #this creates the show instances with the following attributes: :name, :discount
-    Show.create_from_collection(shows_array)
-  end
-
-
+  
   def self.scrape_mainpage
 		html = open(https://broadwayforbrokepeople.com/)
    	page = Nokogiri::HTML(open(https://broadwayforbrokepeople.com/)
