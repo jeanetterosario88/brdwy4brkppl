c44813b79a04d67c4f16b615033891f50cb7fcec
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-05-17 15:17:12 +0000

    first commit

--- brdwy4brkppl/lib/brdwy4brkppl.rb
+++ brdwy4brkppl/lib/brdwy4brkppl.rb
@@ -4,3 +4,25 @@ module Brdwy4brkppl
   class Error < StandardError; end
   # Your code goes here...
 end
+require 'open-uri'
+require 'pry'
+
+class Scraper
+
+  def self.scrape_mainpage_and_create
+    page = Nokogiri::HTML(open(index_url))
+    allshows = []
+    page.css(“  CLASS ”).each do |show|
+     name = show.css(“.CLASS”).text
+k = Song.
+     Show.new.name = name
+     discountinfo = show.css(“CLASS”).text
+     Show.new.discount = discountinfo
+     show_information = {:name => name,
+                :discountinfo => discountinfo}
+      allshows << show_information
+      end
+    allshows
+   end
+
+end
