f181ff9538873490f39b70787ca869ad9e2e2f50
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-05-17 16:07:09 +0000

    Show details

--- brdwy4brkppl/lib/Show.rb
+++ brdwy4brkppl/lib/Show.rb
@@ -1,28 +1,40 @@
 require "brdwy4brkppl/version"
 
-module Brdwy4brkppl
-  class Error < StandardError; end
-  # Your code goes here...
+class Show
+  
+  attr_accessor :name :discount
+  
+  @@all = []
+  
+  def initialize(name, discount)
+    @name = name
+    @discount = discount
 end
-require 'open-uri'
-require 'pry'
+  
+  def name
+    @name
+  end
+  
+  def discount
+    @discount
+  end
 
-class Scraper
-
-  def self.scrape_mainpage_and_create
-    page = Nokogiri::HTML(open(index_url))
-    allshows = []
-    page.css(“  CLASS ”).each do |show|
-     name = show.css(“.CLASS”).text
-k = Song.
-     Show.new.name = name
-     discountinfo = show.css(“CLASS”).text
-     Show.new.discount = discountinfo
-     show_information = {:name => name,
-                :discountinfo => discountinfo}
-      allshows << show_information
-      end
-    allshows
-   end
-
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def self.create(name)
+    newshow = Show.new(name)
+    newshow.save
+    newsong
+  end
+  
 end
